501d6eebca04c38641bf1c18eaadde5190e72b12
Pintta <69728770+Pintta@users.noreply.github.com>
2024-02-24 18:07:58 +0200

    Update math.lua

--- math.lua
+++ math.lua
@@ -1,6 +1,6 @@
-local TimerA = (1000 * 60) * 60 -- 1 hour
-local TimerB = (1000 * 60) * 10 -- 10 minutes
-local TimerC = (1000 * 60) * 1 -- 1 minute
+local TimerA = ((1000 * 60) * 60) -- 1 hour
+local TimerB = ((1000 * 60) * 10) -- 10 minutes
+local TimerC = ((1000 * 60) * 1) -- 1 minute
 
 local TimerLong = (1000 * 60) * (60 * 2) -- 2 hours
 
