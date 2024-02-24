e53c594932632d30b3c60725139a0b5bba150415
Pintta <69728770+Pintta@users.noreply.github.com>
2024-02-24 18:04:53 +0200

    Create math.lua

---
+++ math.lua
@@ -0,0 +1,12 @@
+local TimerA = (1000 * 60) * 60 -- 1 hour
+local TimerB = (1000 * 60) * 10 -- 10 minutes
+local TimerC = (1000 * 60) * 1 -- 1 minute
+
+local TimerLong = (1000 * 60) * (60 * 2) -- 2 hours
+
+local ExampleA = (1000 -- 1000 milliseconds
+  local ExampleB = (1000 * 60 -- 60 seconds
+    local ExampleC = ((1000 * 60) -- First math area close (works), if you have area math is not works..
+      local ExampleD = ((1000 * 60) * 60) -- 1000 milliseconds X 60 seconds x 60 minutes = 1 hour
+
+Wait(50) -- 50 milliseconds waiting time (This is so safe)
